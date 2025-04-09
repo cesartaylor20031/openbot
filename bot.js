7d4fb6d9c02f5ba2bbafb2fb4a0c15a0d44bd1bd
cesartaylor20031 <cesartaylor20031@gmail.com>
2025-04-08 23:50:01 -0600

    Update3 bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors"); // 💥 NUEVO
 const puppeteer = require("puppeteer");
 const axios = require("axios");
 
 const app = express();
+app.use(cors()); // 💥 ¡ACTIVANDO EL PERMISO GLOBAL!
 app.use(express.json());
 
 const preguntasPorPaciente = {};
